797950ac77a7a6bd01e6c47b74791d197b0f9cf0
Alberto Mori <albi.mori@gmail.com>
2014-01-18 12:04:03 +0100

    Added Exceptions

--- src/PhpDDD/Domain/Exceptions/Exception.php
+++ src/PhpDDD/Domain/Exceptions/Exception.php
@@ -7,3 +7,21 @@ class CouldNotCreateEntityException extends Exception{
 		parent::__construct($message, $code);
 	}
 }
+
+class EntityNotSavedException extends Exception{
+	function __construct(string $message="", int $code=0){
+		parent::__construct($message, $code);
+	}
+}
+
+class EntityNotDeletedException extends Exception{
+	function __construct(string $message="", int $code=0){
+		parent::__construct($message, $code);
+	}
+}
+
+class NoEntityFoundException extends Exception{
+	function __construct(string $message="", int $code=0){
+		parent::__construct($message, $code);
+	}
+}
